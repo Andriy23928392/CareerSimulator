7614a7cf7a6044b7d8a252f79f273ff212672c40
Andriy23928392 <polupanovandrej53@gmail.com>
2026-04-18 15:28:20 +0300

    Initial commit: Setup solution structure and Player domain model

---
+++ CareerSimulator.Domain/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace CareerSimulator.Domain.Models
+{
+    public enum Position
+    {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+
+    public class Player
+    {
+        public string Name { get; private set; }
+        public Position PlayerPosition { get; private set; }
+
+        public int Energy { get; private set; }
+        public decimal Money { get; private set; }
+
+        public int OverallRating { get; private set; }
+
+        public Player(string name, Position position)
+        {
+            Name = name;
+            PlayerPosition = position;
+
+
+            Energy = 100;
+            Money = 500m;
+            OverallRating = 40;
+        }
+
+        public void SpendEnergy(int amount)
+        {
+            if (Energy - amount < 0)
+            {
+                throw new Exception("Недостатньо енергії для цієї дії!");
+            }
+            Energy -= amount;
+        }
+
+        public void Rest()
+        {
+            Energy = 100;
+        }
+    }
+}
